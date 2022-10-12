49b117a432914b339702884db1971ba8f05190fd
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-10-13 00:11:16 +0900

    演習：表示項目の追加

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <ul>
         <li v-for="user in users" :key="user.id">
-          {{ user.id }},{{ user.name }}
+          {{ user.id }},{{ user.name }},{{ user.company.name }}
         </li>
       </ul>
     </div>
